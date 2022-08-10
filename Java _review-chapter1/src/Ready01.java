4b58941a66db5aa9f41cdd16dcb4efd3c13d7cb0
laemdfk <kappa021618@icloud.com>
2022-08-11 00:23:30 +0900

    first commit

---
+++ Java _review-chapter1/src/Ready01.java
@@ -0,0 +1,12 @@
+
+public class Ready01 {
+ public static void main(String[] args) {
+	System.out.println("Hello,Java");
+ }
+}
+
+//public static void main(String[] args)と入力できていないと、
+//Javaとして認識されないので注意
+//(インデントにも要注意)
+
+//自分で書いたコード→→	public static void main(String args) {
